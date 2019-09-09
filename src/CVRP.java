15cb63214df2a3040a79c4339f7c911ac686ebdf
nikhil keswaney <nikhilkeswaney1996@gmail.com>
2019-09-09 02:42:57 -0400

    created a new class for reading files

--- src/CVRP.java
+++ src/CVRP.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class CVRP {
@@ -7,7 +5,6 @@ public class CVRP {
     private static NodeManager nodeManager = null;
     private static TruckManager truckManager = null;
     private static int optimalValue = 0;
-    private static EgdeType egdeType;
     public static void main(String[] args) throws IOException {
         if(args.length < 1) {
             usage();
@@ -15,88 +12,35 @@ public class CVRP {
         }
 
         String dataSet = args[0];
-        readFile(dataSet);
+        FileReader f = new FileReader(dataSet);
+        f.readFile();
+        System.out.println("test");
     }
 
-    private static void readFile(String dataSet) throws IOException {
-        String currentLine;
-        int size = 0;
-        BufferedReader objReader = new BufferedReader(new FileReader(dataSet));
-        while ((currentLine = objReader.readLine()) != null){
-            String[] currentLineSeperated = currentLine.split(":");
-            String tag = currentLineSeperated[0].strip();
-            switch (tag){
-                case "COMMENT":{
-                    String[] truckNumber = currentLineSeperated[2].split(",");
-                    truckManager = new TruckManager(Integer.parseInt(truckNumber[0].strip()));
-                    optimalValue = Integer.parseInt(currentLineSeperated[3].strip().replace(")", ""));
-                    break;
-                }
-                case "DIMENSION": {
-                    size = Integer.parseInt(currentLineSeperated[1].strip());
-                    break;
-                }
-                case "EDGE_WEIGHT_TYPE":{
-                    String distanceType = currentLineSeperated[1].strip();
-                    if(distanceType.equals("EUC_2D") || distanceType.equals("EUC_3D")) {
-                        egdeType = EgdeType.Euclidean;
-                        nodeManager = new NodeManager(new Euclidean(), size);
-                    }
-                    else if(distanceType.equals("MAN_2D") || distanceType.equals("MAN_3D")) {
-                        egdeType = EgdeType.Euclidean;
-                        nodeManager = new NodeManager(new Manhattan(), size);
-                    }
-                    else if(distanceType.equals("MAX_2D") || distanceType.equals("MAX_3D")) {
-                        egdeType = EgdeType.Max;
-                        nodeManager = new NodeManager(new Max(), size);
-                    }
-                    break;
-                }
-
-                case "NODE_COORD_SECTION":{
-                    String[] nodesCordinates = getChunk(objReader, size);
-                    nodeManager.createNodes(nodesCordinates);
-                    break;
-                }
-
-                case "DEMAND_SECTION":{
-                    String[] demand = getChunk(objReader, size);
-                    nodeManager.setDemands(demand);
-                    break;
-                }
-
-                case "DEPOT_SECTION": {
-                    currentLine = objReader.readLine();
-                    nodeManager.setDepot(Integer.parseInt(currentLine.strip()) - 1);
-                    break;
-                }
-            }
-        }
-
+    public static TruckManager getTruckManager() {
+        return truckManager;
     }
 
-    public static String[] getChunk(BufferedReader objReader, int size) throws IOException {
-        String currentLine;
-        String[] chunk = new String[size];
-        for (int i = 0; i < size; i++){
-            currentLine = objReader.readLine();
-            chunk[i] = currentLine;
-        }
-        return chunk;
+    public static void setTruckManager(TruckManager truckManager) {
+        CVRP.truckManager = truckManager;
+    }
+
+    public static NodeManager getNodeManager() {
+        return nodeManager;
+    }
+
+    public static void setNodeManager(NodeManager nodeManager) {
+        CVRP.nodeManager = nodeManager;
     }
 
     public static int getOptimalValue() {
         return optimalValue;
     }
 
-    public TruckManager getTruckManager() {
-        return truckManager;
+    public static void setOptimalValue(int optimalValue) {
+        CVRP.optimalValue = optimalValue;
     }
 
-    public NodeManager getNodeManager() {
-        return nodeManager;
-    }
-    
     private static void usage() {
         System.out.println("Usage: java CVRP <dataset>");
         System.out.println("1. <dataset> The dataset you want to use");
---
+++ src/FileReader.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class FileReader {
+    private static EgdeType egdeType;
+    public String dataSet;
+    public FileReader(String dataset){
+        this.dataSet = dataset;
+    }
+    public void readFile() throws IOException {
+        String currentLine;
+        int size = 0;
+        BufferedReader objReader = new BufferedReader(new java.io.FileReader(this.dataSet));
+        while ((currentLine = objReader.readLine()) != null){
+            String[] currentLineSeperated = currentLine.split(":");
+            String tag = currentLineSeperated[0].strip();
+            switch (tag){
+                case "COMMENT":{
+                    String[] truckNumber = currentLineSeperated[2].split(",");
+                    CVRP.setTruckManager(new TruckManager(Integer.parseInt(truckNumber[0].strip())));
+                    CVRP.setOptimalValue(Integer.parseInt(currentLineSeperated[3].strip().replace(")", "")));
+                    break;
+                }
+                case "DIMENSION": {
+                    size = Integer.parseInt(currentLineSeperated[1].strip());
+                    break;
+                }
+                case "EDGE_WEIGHT_TYPE":{
+                    String distanceType = currentLineSeperated[1].strip();
+                    if(distanceType.equals("EUC_2D") || distanceType.equals("EUC_3D")) {
+                        egdeType = EgdeType.Euclidean;
+                        CVRP.setNodeManager(new NodeManager(new Euclidean(), size));
+                    }
+                    else if(distanceType.equals("MAN_2D") || distanceType.equals("MAN_3D")) {
+                        egdeType = EgdeType.Euclidean;
+                        CVRP.setNodeManager(new NodeManager(new Manhattan(), size));
+                    }
+                    else if(distanceType.equals("MAX_2D") || distanceType.equals("MAX_3D")) {
+                        egdeType = EgdeType.Max;
+                        CVRP.setNodeManager(new NodeManager(new Max(), size));
+                    }
+                    break;
+                }
+
+                case "NODE_COORD_SECTION":{
+                    String[] nodesCordinates = getChunk(objReader, size);
+                    CVRP.getNodeManager().createNodes(nodesCordinates);
+                    break;
+                }
+
+                case "DEMAND_SECTION":{
+                    String[] demand = getChunk(objReader, size);
+                    CVRP.getNodeManager().setDemands(demand);
+                    break;
+                }
+
+                case "DEPOT_SECTION": {
+                    currentLine = objReader.readLine();
+                    CVRP.getNodeManager().setDepot(Integer.parseInt(currentLine.strip()) - 1);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static String[] getChunk(BufferedReader objReader, int size) throws IOException {
+        String currentLine;
+        String[] chunk = new String[size];
+        for (int i = 0; i < size; i++){
+            currentLine = objReader.readLine();
+            chunk[i] = currentLine;
+        }
+        return chunk;
+    }
+}
